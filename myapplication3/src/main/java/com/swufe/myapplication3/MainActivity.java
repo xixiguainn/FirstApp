5f4f199e43a550d5507b4050b566c1ab799ddd3f
Gaojunyu <1481369958@qq.com>
2020-03-21 13:52:50 +0800

    温度转换器

--- myapplication3/src/main/java/com/swufe/myapplication3/MainActivity.java
+++ myapplication3/src/main/java/com/swufe/myapplication3/MainActivity.java
@@ -3,12 +3,36 @@ package com.swufe.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView out;
+    EditText inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    out=(TextView) findViewById(R.id.textView2);
+    inp=(EditText) findViewById(R.id.editText);
+
+    Button btn=(Button)findViewById(R.id.button2);
+
+
     }
+public void btnClick(View btn){
+
+String str=inp.getText().toString();
+    int number=Integer.parseInt( str);
+    out.setText("结果为"+(number*9/5+32)+"°F");
+
+
+
+    }
+
+
 }
+
