cb4567448130bd216a0a2126c4d6ffabe2c229e9
Gaojunyu <1481369958@qq.com>
2020-05-31 18:46:17 +0800

    计分系统

--- myapplication3/src/main/java/com/swufe/myapplication3/MainActivity.java
+++ myapplication3/src/main/java/com/swufe/myapplication3/MainActivity.java
@@ -11,21 +11,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView out;
     EditText inp;
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    out=(TextView) findViewById(R.id.textView2);
-    inp=(EditText) findViewById(R.id.editText);
+        out =(TextView) findViewById(R.id.textView2);//从view里查找id 是view类型要转化为TextView型
+        inp =(EditText) findViewById(R.id.editText);
 
-    Button btn=(Button)findViewById(R.id.button2);
+        btn=(Button)findViewById(R.id.button2);
 
 
     }
 public void btnClick(View btn){
 
-String str=inp.getText().toString();
+String str=inp.getText().toString();//强制转化成string型
     int number=Integer.parseInt( str);
     out.setText("结果为"+(number*9/5+32)+"°F");
 
