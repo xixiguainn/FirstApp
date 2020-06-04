3be66c8886f07a45e222b5d465557d663c5bf18a
Gaojunyu <1481369958@qq.com>
2020-06-04 22:00:54 +0800

    完成汇率保存

--- myapplication3/src/main/java/com/swufe/myapplication3/Main3Activity.java
+++ myapplication3/src/main/java/com/swufe/myapplication3/Main3Activity.java
@@ -3,7 +3,9 @@ package com.swufe.myapplication3;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,8 +32,16 @@ public class Main3Activity extends AppCompatActivity {
    dollar=(Button)findViewById(R.id.dollar);
    europe=(Button)findViewById(R.id.europe);
    korea=(Button)findViewById(R.id.hanguo);
-
+        SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        //获取sp里保存的数据
+        dollarRate=sharedPreferences.getFloat("dollar_rate",0.0f);
+        europeRate=sharedPreferences.getFloat("europe_rate",0.0f);
+        koreaRate=sharedPreferences.getFloat("korea_rate",0.0f);
     }
+
+
+
+
     public void onClick(View btn) {
         Float value=0f;
        String str=put.getText().toString();
@@ -74,6 +84,12 @@ else if (btn.getId()==R.id.europe){
             europeRate =bundle.getFloat("key_europe",0.1f);
             koreaRate =bundle.getFloat("key_korea",0.1f);
 
+            SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putFloat("dollar_rate",dollarRate);
+            editor.putFloat("europe_rate",europeRate);
+            editor.putFloat("korea_rate",koreaRate);
+            editor.commit();//将索引对应的值传给对应的变量
         }
 
 
